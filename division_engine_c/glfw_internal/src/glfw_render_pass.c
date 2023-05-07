c8e7f90885c20be4ae463dfc649d025b92fd9461
Den163 <abramov.denis163@gmail.com>
2023-05-08 00:29:23 +0400

    Updated glfw according to osx changes

---
+++ division_engine_c/glfw_internal/src/glfw_render_pass.c
@@ -0,0 +1,16 @@
+#include "division_engine/platform_internal/platform_render_pass.h"
+
+bool division_engine_internal_platform_render_pass_context_alloc(
+    DivisionContext* ctx, const DivisionSettings* settings)
+{
+    return true;
+}
+
+void division_engine_internal_platform_render_pass_context_free(DivisionContext* ctx)
+{
+}
+
+bool division_engine_internal_platform_render_pass_alloc(DivisionContext* ctx, DivisionRenderPass* render_pass)
+{
+    return true;
+}
